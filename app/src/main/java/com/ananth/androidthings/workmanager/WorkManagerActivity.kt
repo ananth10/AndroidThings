13351e2276693169a8209372ba63994f82abc8d5
Ananth <ananthababu.vannimuthu@abbvie.com>
2023-09-02 11:15:42 +0530

    work manager

---
+++ app/src/main/java/com/ananth/androidthings/workmanager/WorkManagerActivity.kt
@@ -0,0 +1,106 @@
+package com.ananth.androidthings.workmanager
+
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Text
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.work.Constraints
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.workDataOf
+import coil.compose.AsyncImage
+import com.ananth.androidthings.ui.theme.AndroidThingsTheme
+
+class WorkManagerActivity : ComponentActivity() {
+
+    private lateinit var workManager: WorkManager
+    private val viewModel by viewModels<PhotoViewModel>()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        workManager = WorkManager.getInstance(applicationContext)
+        var count = 100
+        do {
+            count -= 5
+            print("count: $count")
+        } while (count > 20)
+
+        setContent {
+            AndroidThingsTheme {
+                val workResult = viewModel.workId?.let { id ->
+                    workManager.getWorkInfoByIdLiveData(id).observeAsState().value
+                }
+                LaunchedEffect(key1 = workResult?.outputData) {
+                    if (workResult?.outputData != null) {
+                        val filePath =
+                            workResult.outputData.getString(PhotoCompressionWorker.KEY_RESULT_PATH)
+                        filePath?.let {
+                            val bitMap = BitmapFactory.decodeFile(it)
+                            viewModel.updateCompressedBitmap(bitMap)
+                        }
+                    }
+                }
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    viewModel.uncompressedUri?.let {
+                        Text(text = "UnCompressed photo")
+                        AsyncImage(model = it, contentDescription = null)
+                    }
+                    Spacer(modifier = Modifier.height(10.dp))
+                    viewModel.compressedBitmap?.let {
+                        Text(text = "Compressed photo")
+                        Image(bitmap = it.asImageBitmap(), contentDescription = null)
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        val uri = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            intent?.getParcelableExtra(Intent.EXTRA_STREAM, Uri::class.java)
+        } else {
+            intent?.getParcelableExtra(Intent.EXTRA_STREAM)
+        } ?: return
+
+        viewModel.updateUnCompressedUri(uri)
+
+        val request = OneTimeWorkRequestBuilder<PhotoCompressionWorker>()
+            .setInputData(
+                workDataOf(
+                    PhotoCompressionWorker.KEY_CONTENT_URI to uri.toString(),
+                    PhotoCompressionWorker.KEY_COMPRESSION_THRESHOLD to 1024 * 20L
+                )
+            )
+//            .setConstraints(
+//                Constraints(
+//                    requiresStorageNotLow = true
+//                )
+//            )
+            .build()
+
+        viewModel.updateWorkId(request.id)
+        workManager.enqueue(request)
+    }
+}
